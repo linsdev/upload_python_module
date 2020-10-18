8745fbfa715439fb476c49af4691b1aeceede053
Nazar Litvin <nazarlitvin@outlook.com>
2020-10-18 17:50:23 +0300

    Uploader class implementation

--- net/fileoperations/uploader.py
+++ net/fileoperations/uploader.py
@@ -1,21 +1,48 @@
+import threading
+import queue
+
 from . import datatypes
+from . import uploader_func
 
 
 class Uploader:
     def __init__(self, filenames, number_of_processes, loading_progress):
-        pass
+        self._stop_event = threading.Event()
+        self._stop_event.set()
+
+        self._filenames = filenames
+        self._uploaded_files = []
+
+        imap_next_thread_iter_queue = queue.Queue()
+        imap_next_thread_result_queue = queue.Queue()
+
+        self._imap_next_thread = threading.Thread(
+            target=uploader_func.imap_next_thread,
+            args=(imap_next_thread_iter_queue, imap_next_thread_result_queue)
+        )
+
+        self._processing_thread = threading.Thread(
+            target=uploader_func.processing_thread,
+            args=(filenames, number_of_processes, loading_progress,
+                  imap_next_thread_iter_queue, imap_next_thread_result_queue,
+                  self._stop_event, self._uploaded_files)
+        )
 
     def start(self):
-        pass
+        self._imap_next_thread.start()
+        self._processing_thread.start()
 
     def stop(self):
-        pass
+        self._stop_event.set()
 
     def get_report(self):
-        return datatypes.UploadReport([], [])
+        return datatypes.UploadReport(
+            uploaded_files=self._uploaded_files,
+            not_uploaded_files=list(set(self._filenames) - set(self._uploaded_files))
+        )
 
     def is_active(self):
-        return False
+        return not self._stop_event.is_set()
 
     def wait_for_uploaded(self):
-        pass
+        self._stop_event.wait()
---
+++ net/fileoperations/uploader_func.py
@@ -0,0 +1,7 @@
+def imap_next_thread(iter_queue, result_queue):
+    pass
+
+
+def processing_thread(filenames, number_of_processes, loading_progress_queue,
+                      iter_queue, result_queue, stop_event, uploaded_files):
+    pass
