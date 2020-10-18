0c0d8fe951f4fc10e9f1d6ea4979e5cab55bffbe
Nazar Litvin <nazarlitvin@outlook.com>
2020-10-18 17:11:29 +0300

    Module mock

---
+++ net/fileoperations/uploader.py
@@ -0,0 +1,18 @@
+class Uploader:
+    def __init__(self, filenames, number_of_processes, loading_progress):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def get_report(self):
+        return None
+
+    def is_active(self):
+        return False
+
+    def wait_for_uploaded(self):
+        pass
