ec188c38fdab565f99c69f9dc56a52a93c2b5666
Nazar Litvin <nazarlitvin@outlook.com>
2020-10-18 17:32:57 +0300

    Test Cases

--- net/fileoperations/unittest.py
+++ net/fileoperations/unittest.py
@@ -38,6 +38,26 @@ class UploaderCase(unittest.TestCase):
 
         return UploaderCase.mock(files_list, number_of_processes, f)
 
+    def stop_after_N_seconds(self, N):
+        report, t = self.mock_stop_after_N_seconds(self.filenames, self.number_of_processes, N)
+        self.assertLessEqual(t, N + self.time_to_stop)
+
+    def test_stop_immediately(self):
+        self.stop_after_N_seconds(0)
+
+    def test_stop_after_2_seconds(self):
+        self.stop_after_N_seconds(2)
+
+    def test_upload(self):
+        report, progress = self.mock_wait_while_is_active(self.filenames, self.number_of_processes)
+
+        last_progress = progress[-1]
+        self.assertEqual(last_progress.done, last_progress.total)
+        self.assertEqual(last_progress.error, 0)
+
+        self.assertEqual(sorted(report.uploaded_files), self.filenames)
+        self.assertEqual(report.not_uploaded_files, [])
+
 
 if __name__ == '__main__':
     unittest.main()
